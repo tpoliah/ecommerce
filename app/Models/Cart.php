8ca11d977dddd35d6cfd523e6887b60573f7dbd2
tpoliah <tigannapoliah28@gmail.com>
2024-03-13 21:00:47 -0400

    Add to cart from store page and item removal from cart.

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+    protected $table = 'cart';
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+    ];
 }
