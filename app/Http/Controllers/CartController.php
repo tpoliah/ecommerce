ecf315b75d30c73f7fca79acbb3e01b8a8a3f31b
tpoliah <tigannapoliah28@gmail.com>
2024-03-13 20:59:51 -0400

    Apply to cart from store page and item removal from cart.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Models\Cart;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
@@ -18,5 +19,36 @@ class CartController extends Controller
         //  dd($cart_data);
 
         return view('pages.default.cartpage', compact('cart_data'));
+    }
+
+    public function store(Request $request)
+    {
+        Cart::updateOrCreate(
+            ['user_id' => Auth::id(), 'product_id' => $request->product_id],
+            ['quantity' => DB::raw('quantity + '.$request->quantity), 'updated_at' => now()]
+        );
+
+        return redirect()->route('cart.index')->with('message', 'Product added to cart');
+    }
+
+    public function addToCartFromStore(Request $request)
+    {
+        Cart::updateOrCreate(
+            ['user_id' => Auth::id(), 'product_id' => $request->id],
+            ['quantity' => DB::raw('quantity + '. 1), 'updated_at' => now()]
+        );
+
+        return redirect()->route('cart.index')->with('message', 'Product added to cart');
+    }
+
+    public function destroy(Request $request)
+    {
+        Cart::destroy($request->id);
+
+        // Cart::where('id', $request->id)
+        // ->where('user_id', Auth::id())
+        // ->delete();
+
+        return redirect()->route('cart.index')->with('message', 'Product removed from cart');
     }
 }
