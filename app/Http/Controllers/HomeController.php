85473d910ab73a9f2ccff487d135a90835d43e2f
tpoliah <tigannapoliah28@gmail.com>
2024-07-02 19:29:43 -0400

    Special Deals Menu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $specialDealProducts = Product::where('quantity', '>', 0)
+            ->inRandomOrder()
+            ->take(4)
+            ->get();
+
+        return view('home', compact('specialDealProducts'));
+
+        // return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,138 +28,52 @@
             <div class="row justify-content-center mb-3 pb-3">
                 <div class="col-md-12 heading-section text-center ftco-animate">
                     <h2 class="mb-4">Special Deals</h2>
-                    <p>Checkout out our items that are on sale.</p>
+                    <p>Here are some products that are on sale just for you.</p>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-sm col-md-6 col-lg ftco-animate">
-                    <div class="product">
-                        <a href="#" class="img-prod"><img class="img-fluid" src="http://localhost:8000/storage//images/products/product1-1.jpg"
-                                alt="Colorlib Template">
-                            <span class="status">30%</span>
-                            <div class="overlay"></div>
-                        </a>
-                        <div class="text py-3 px-3">
-                            <h3><a href="#">Sweet Peppers</a></h3>
-                            <div class="d-flex">
-                                <div class="pricing">
-                                    <p class="price"><span class="mr-2 price-dc">$10.00</span><span
-                                            class="price-sale">$7.00</span></p>
-                                </div>
-                                <div class="rating">
-                                    <p class="text-right">
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-                            <p class="bottom-area d-flex px-3">
-                                <a href="#" class="add-to-cart text-center py-2 mr-1"><span>Add to cart <i
-                                            class="ion-ios-add ml-1"></i></span></a>
-                                <a href="#" class="buy-now text-center py-2">Buy now<span><i
-                                            class="ion-ios-cart ml-1"></i></span></a>
-                            </p>
-                        </div>
-                    </div>
+            @foreach ($specialDealProducts as $product)
+
+<div class="col-sm-6 col-md-6 col-lg-3 ftco-animate">
+    <div class="product">
+        <a href="{{ $product->getLink() }}" class="img-prod"><img class="img-fluid"
+                src="{{ $product->getImage() }}" alt="Colorlib Template">
+            {{-- <span class="status">30%</span> --}}
+            <div class="overlay"></div>
+        </a>
+        <div class="text py-3 px-3">
+            <h3><a href="{{ $product->getLink() }}">{{ $product->title }}</a></h3>
+            <div class="d-flex">
+                <div class="pricing">
+                    <p class="price">
+                        {{-- <span class="mr-2 price-dc">$120.00</span> --}}
+
+                        <span class="price-sale">${{ $product->getPrice() }}</span>
+                    </p>
                 </div>
-                <div class="col-sm col-md-6 col-lg ftco-animate">
-                    <div class="product">
-                        <a href="#" class="img-prod"><img class="img-fluid" src="http://localhost:8000/storage//images/products/product1-5.jpg"
-                                alt="Colorlib Template">
-                            <div class="overlay"></div>
-                        </a>
-                        <div class="text py-3 px-3">
-                            <h3><a href="#">Sweet Tomato</a></h3>
-                            <div class="d-flex">
-                                <div class="pricing">
-                                    <p class="price"><span>$30.00</span></p>
-                                </div>
-                                <div class="rating">
-                                    <p class="text-right">
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-                            <p class="bottom-area d-flex px-3">
-                                <a href="#" class="add-to-cart text-center py-2 mr-1"><span>Add to cart <i
-                                            class="ion-ios-add ml-1"></i></span></a>
-                                <a href="#" class="buy-now text-center py-2">Buy now<span><i
-                                            class="ion-ios-cart ml-1"></i></span></a>
-                            </p>
-                        </div>
-                    </div>
+                <div class="rating">
+                    <p class="text-right">
+                        <a href="#"><span class="ion-ios-star-outline"></span></a>
+                        <a href="#"><span class="ion-ios-star-outline"></span></a>
+                        <a href="#"><span class="ion-ios-star-outline"></span></a>
+                        <a href="#"><span class="ion-ios-star-outline"></span></a>
+                        <a href="#"><span class="ion-ios-star-outline"></span></a>
+                    </p>
                 </div>
-                <div class="col-sm col-md-6 col-lg ftco-animate">
-                    <div class="product">
-                        <a href="#" class="img-prod"><img class="img-fluid" src="http://localhost:8000/storage//images/products/product1-9.jpg"
-                                alt="Colorlib Template">
-                            <div class="overlay"></div>
-                        </a>
-                        <div class="text py-3 px-3">
-                            <h3><a href="#">Apples</a></h3>
-                            <div class="d-flex">
-                                <div class="pricing">
-                                    <p class="price"><span>$3.00</span></p>
-                                </div>
-                                <div class="rating">
-                                    <p class="text-right">
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-                            <p class="bottom-area d-flex px-3">
-                                <a href="#" class="add-to-cart text-center py-2 mr-1"><span>Add to cart <i
-                                            class="ion-ios-add ml-1"></i></span></a>
-                                <a href="#" class="buy-now text-center py-2">Buy now<span><i
-                                            class="ion-ios-cart ml-1"></i></span></a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm col-md-6 col-lg ftco-animate">
-                    <div class="product">
-                        <a href="#" class="img-prod"><img class="img-fluid" src="http://localhost:8000/storage//images/products/product1-4.jpg"
-                                alt="Colorlib Template">
-                            <div class="overlay"></div>
-                        </a>
-                        <div class="text py-3 px-3">
-                            <h3><a href="#">Red Cabbage</a></h3>
-                            <div class="d-flex">
-                                <div class="pricing">
-                                    <p class="price"><span>$20.00</span></p>
-                                </div>
-                                <div class="rating">
-                                    <p class="text-right">
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                        <a href="#"><span class="ion-ios-star-outline"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-                            <p class="bottom-area d-flex px-3">
-                                <a href="#" class="add-to-cart text-center py-2 mr-1"><span>Add to cart <i
-                                            class="ion-ios-add ml-1"></i></span></a>
-                                <a href="#" class="buy-now text-center py-2">Buy now<span><i
-                                            class="ion-ios-cart ml-1"></i></span></a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+            </div>
+            <p class="bottom-area d-flex px-3">
+                <a href="{{ route('cart.addfromstorepage', ['id' => $product->id]) }}" 
+                    class="add-to-cart text-center py-2 mr-1"><span>Add to cart <i
+                            class="ion-ios-add ml-1"></i></span></a>
+                <a href="{{ $product->getLink() }}" class="buy-now text-center py-2">View<span><i
+                            class="ion-ios-cart ml-1"></i></span></a>
+            </p>
+        </div>
+    </div>
+</div>
+@endforeach
             </div>
         </div>
     </section>
