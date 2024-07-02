d085ceecd8c85a85a5153c1c7588f0287c694530
tpoliah <tigannapoliah28@gmail.com>
2024-07-02 18:59:32 -0400

    Related Products Menu

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class DetailController extends Controller
 {
     public function index($id)
     {
-
         $data = Product::singleProduct($id)->withPrices()->get()->first();
 
-        return view('pages.default.detailspage', compact('data'));
-        return view('pages.testing.detailspage', compact('data'));
+        $currentProduct = Product::findOrFail($id);
+
+        $relatedProducts = Product::where('quantity', '>', 0)
+            ->where('id', '!=', $id)
+            ->inRandomOrder()
+            ->take(4)
+            ->get();
+
+        return view('pages.default.detailspage', compact('data', 'relatedProducts', 'currentProduct'));
+
+        return view('pages.testing.detailspage', compact('data', 'relatedProducts', 'currentProduct'));
     }
 }
--- resources/views/pages/default/checkout-successpage.blade.php
+++ resources/views/pages/default/checkout-successpage.blade.php
@@ -1,11 +1,12 @@
 <x-mylayouts.layout-default>
-
 <div class="my-5 container">
     <div class="jumbotron">
     <h1>Your purchase was successful</h1>  
     </div>
 
 </div>
-
-
 </x-mylayouts.layout-default>
+
+
+
+
